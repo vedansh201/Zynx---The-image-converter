9a8797f600d3760403ed84df76aa58e3c6f34e61
vedansh201 <notvedansh2010@gmail.com>
2026-07-10 21:32:41 +0530

    Main.py

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
+import sys
+from PyQt6.QtWidgets import QApplication
+from styles import DARK_STYLE
 
+from ui import MainWindow
+
+
+
+app = QApplication(sys.argv)
+app.setStyleSheet(DARK_STYLE)
+
+window = MainWindow()
+window.show()
+
+sys.exit(app.exec())
